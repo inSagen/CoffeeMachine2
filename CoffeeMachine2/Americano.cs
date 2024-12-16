903933565a73a59420bc057048b309140a119f79
valiullin.i <valiullininsaf2@gmail.com>
2024-12-16 07:01:06 +0300

    начал, но не закончил

---
+++ CoffeeMachine2/Americano.cs
@@ -0,0 +1,6 @@
+﻿namespace CoffeeMachine2;
+
+public class Americano
+{
+    
+}
---
+++ CoffeeMachine2/CoffeeMachine.cs
@@ -0,0 +1,28 @@
+﻿namespace CoffeeMachine2;
+
+public class CoffeeMachine
+{
+    enum CoffeeMenuEnum
+    {
+        Latte = 1,
+        Espresso,
+        Americano,
+        Cappuccino,
+        Mocha
+    }
+
+    private string[] CoffeeTypes = {"Латте", "Эспрессо", "Американо", "Капучино", "Мокко"};
+
+    public void ShowCoffeeMenu()
+    {
+        for (int i = 0; i < CoffeeTypes.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {CoffeeTypes[i]}");
+        }
+    }
+
+    public virtual void MakeCoffee(int userCoffeeChoice, int userSugarChoice)
+    {
+        Console.WriteLine("Готовим кофе, добавляем сахар...");
+    }
+}
